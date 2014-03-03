3f9b462a82dd543db8ba758e56f9e90c0691e9cc
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2014-03-03 21:45:44 +0100

    Tidy. exptected -> expected

--- expectexception.h
+++ expectexception.h
@@ -8,8 +8,8 @@
  * The EXPECT_EXCEPTION macro should ensure that an expression throws an exception of a given type.
  */
 
-class exptected_exception: virtual public boost::exception, virtual public std::exception {};
-class unexptected_exception: virtual public boost::exception, virtual public std::exception {};
+class expected_exception: virtual public boost::exception, virtual public std::exception {};
+class unexpected_exception: virtual public boost::exception, virtual public std::exception {};
 typedef boost::error_info<struct unexpected_exception_tag,boost::exception_ptr> unexpected_exception_info;
 
 typedef boost::error_info<struct tag_expected_exception_type,std::type_info const *> expected_exception_type;
@@ -28,15 +28,15 @@ to_string( expected_exception_type const & x )
         F;               \
     } catch (const X&) { \
         thrown = true;   \
-    } catch (const std::exception& x) { \
-        BOOST_THROW_EXCEPTION(unexptected_exception()  \
+    } catch (const std::exception&) { \
+        BOOST_THROW_EXCEPTION(unexpected_exception()  \
             << unexpected_exception_info(boost::current_exception ()) \
             << Backtrace::make () \
             << expected_exception_type(&typeid(X))); \
     }                    \
                          \
     if (!thrown)         \
-        BOOST_THROW_EXCEPTION(exptected_exception() \
+        BOOST_THROW_EXCEPTION(expected_exception() \
             << Backtrace::make () \
             << expected_exception_type(&typeid(X))); \
 } while(false);
