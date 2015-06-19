7794badcb39845509953def142590fbc50021419
Johan Gustafsson <johan.b.gustafsson@gmail.com>
2015-06-19 11:10:31 +0200

    detectgdb: tidy, and spin waitpid until the child process is finished

--- detectgdb.cpp
+++ detectgdb.cpp
@@ -1,20 +1,27 @@
 #include "detectgdb.h"
 
 #ifndef _MSC_VER
-#include <sys/types.h>
-#include <sys/ptrace.h>
-#include <sys/wait.h>
+    #include <sys/types.h>
+    #ifndef DARWIN_NO_CARBON // skip for ios
+        #include <sys/ptrace.h>
+    #endif
+    #include <sys/wait.h>
 #else
-#include <Windows.h> // IsDebuggerPresent
+    #include <Windows.h> // IsDebuggerPresent
 #endif
 
 #ifndef PTRACE_ATTACH
-#define PTRACE_ATTACH PT_ATTACH
-#define PTRACE_CONT PT_CONTINUE
-#define PTRACE_DETACH PT_DETACH
+    #define PTRACE_ATTACH PT_ATTACH
+    #define PTRACE_CONT PT_CONTINUE
+    #define PTRACE_DETACH PT_DETACH
 #endif
 
 #include <stdio.h>
+#include <errno.h>
+
+#ifdef __APPLE__
+    #include <TargetConditionals.h>
+#endif
 
 #if defined(__GNUC__)
     #include <unistd.h>
@@ -47,28 +54,29 @@ bool is_running_through_gdb_xorl()
 bool is_running_through_gdb_terminus()
 {
   int pid = fork();
-  int status;
+  int status = 0;
   int res;
 
   if (pid == -1)
     {
+      printf("Fork failed!\n");
       perror("fork");
-      return -1;
+      return 1;
     }
 
   if (pid == 0)
     {
+      /* Child fork */
       int ppid = getppid();
 
-      /* Child */
-      if (ptrace(PTRACE_ATTACH, ppid, NULL, NULL) == 0)
+      if (ptrace(PTRACE_ATTACH, ppid, nullptr, 0) == 0)
         {
           /* Wait for the parent to stop and continue it */
-          waitpid(ppid, NULL, 0);
-          ptrace(PTRACE_CONT, ppid, NULL, NULL);
+          waitpid(ppid, nullptr, 0);
+          ptrace(PTRACE_CONT, ppid, nullptr, 0);
 
           /* Detach */
-          ptrace(PTRACE_DETACH, getppid(), NULL, NULL);
+          ptrace(PTRACE_DETACH, ppid, nullptr, 0);
 
           /* We were the tracers, so gdb is not present */
           res = 0;
@@ -78,12 +86,30 @@ bool is_running_through_gdb_terminus()
           /* Trace failed so gdb is present */
           res = 1;
         }
+
       _exit(res);
     }
   else
     {
-      waitpid(pid, &status, 0);
-      res = WEXITSTATUS(status);
+      pid_t w = 0;
+      do
+        {
+          // the first signal might be an unblocked signal, skip it
+          w = waitpid(pid, &status, 0);
+        }
+      while (w < 0 && errno == EINTR);
+
+      // fall-back to return "true" if the fork failed for whatever reason
+      res = WIFEXITED(status) ? WEXITSTATUS(status) : 1;
+
+        //std::cout << "WIFCONTINUED(status)="<< WIFCONTINUED(status)
+        //        << ", WSTOPSIG(status)=" << WSTOPSIG(status) << std::endl;
+        //std::cout << "WIFSTOPPED(status)="<< WIFSTOPPED(status)
+        //        << ", WSTOPSIG(status)=" << WSTOPSIG(status) << std::endl;
+        //std::cout << "WIFSIGNALED(status)="<< WIFSIGNALED(status)
+        //        << ", WTERMSIG(status)=" << WTERMSIG(status) << std::endl;
+        //std::cout << "WIFEXITED(status)="<< WIFEXITED(status)
+        //        << ", WEXITSTATUS(status)=" << WEXITSTATUS(status) << std::endl;
     }
   return res;
 }
@@ -92,10 +118,14 @@ bool is_running_through_gdb_terminus()
 bool DetectGdb::
         is_running_through_gdb()
 {
-    bool is_attached_by_system_debugger = is_running_through_gdb_terminus();
+#if defined(__APPLE_CPP__) && (TARGET_OS_IPHONE==0)
+    // No implementation for detecting IOS debugger
+    return false;
+#else
     bool is_attached_in_qt_creator = is_running_through_gdb_xorl();
-
-    return is_attached_by_system_debugger || is_attached_in_qt_creator;
+    bool is_attached_by_system_debugger = is_running_through_gdb_terminus();
+    return is_attached_in_qt_creator || is_attached_by_system_debugger;
+#endif
 }
 
 #else
